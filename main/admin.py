2939d1f7447a5654b84df30833e18e9b25b9985d
Dmitry <ddmitiy@mail.ru>
2021-12-16 00:36:01 +0300

    Modifed admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Trainig
+
+class TrainigAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Trainig, TrainigAdmin)
